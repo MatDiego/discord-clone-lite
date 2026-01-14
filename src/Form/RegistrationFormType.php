d84a906354dc9f79d8dfb2a3c1629ca59cf6c1dd
Mateusz Nasiadka <matnas2000@wp.pl>
2026-01-14 03:59:15 +0100

    feat(i18n): implement translation keys for registration form
    - Add messages.pl.yaml with registration form labels
    - Update RegistrationFormType to use translation keys instead of hardcoded strings
    - Refactor register.html.twig to rely on form type labels

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,30 +19,44 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('username', null, [
-                'label' => 'Nazwa użytkownika'
+            ->add('username', TextType::class, [
+                'label' => 'register.username',
+                'attr' => [
+                    'placeholder' => 'register.username_placeholder',
+                    'class' => 'form-control',
+                ],
             ])
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('email', EmailType::class, [
+                'label' => 'register.email',
+                'attr' => [
+                    'placeholder' => 'register.email_placeholder',
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'register.password',
+                'attr' => [
+                    'placeholder' => 'register.password_placeholder',
+                    'autocomplete' => 'new-password',
+                ],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        'max' => 4096,
-                    ]),
+                    new NotBlank(
+                        message: 'Proszę podać hasło',
+                    ),
+                    new Length(
+                        min: 6,
+                        max: 4096,
+                        minMessage: 'Hasło musi mieć minimum {{ limit }} znaków',
+                    ),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'register.agree_terms',
+                'constraints' => [
+                    new IsTrue(
+                        message: 'Musisz zaakceptować regulamin.',
+                    ),
                 ],
             ])
         ;
