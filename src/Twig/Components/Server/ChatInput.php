2a014729d83b1db6d1823552475fd16170fc399b
Mateusz Nasiadka <matnas2000@wp.pl>
2026-02-24 17:18:16 +0100

    refactor(templates): enforce permission checks for secure UI elements
    - Updated Twig components and templates to conditionally render elements based on user permissions.
    - Applied 'is_granted' checks to buttons, dropdowns, and inputs to prevent unauthorized interaction attempts.

--- src/Twig/Components/Server/ChatInput.php
+++ src/Twig/Components/Server/ChatInput.php
@@ -4,7 +4,11 @@ namespace App\Twig\Components\Server;
 
 use App\Entity\Channel;
 use App\Entity\Server;
+use App\Entity\User;
+use App\Enum\UserPermissionEnum;
 use App\Form\CreateMessageType;
+use App\Service\PermissionService;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
@@ -15,9 +19,12 @@ class ChatInput
     public Server $server;
     public Channel $channel;
     public FormView $form;
+    public bool $canSendMessages = false;
 
     public function __construct(
-        private readonly FormFactoryInterface $formFactory
+        private readonly FormFactoryInterface $formFactory,
+        private readonly PermissionService $permissionService,
+        private readonly Security $security,
     ) {
     }
 
@@ -26,6 +33,15 @@ class ChatInput
         $this->server = $server;
         $this->channel = $channel;
 
+        $user = $this->security->getUser();
+        if ($user instanceof User) {
+            $this->canSendMessages = $this->permissionService->hasChannelPermission(
+                $user,
+                $channel,
+                UserPermissionEnum::SEND_MESSAGES
+            );
+        }
+
         $this->form = $this->formFactory
             ->create(CreateMessageType::class, null, [
                 'channel' => $channel
@@ -33,3 +49,4 @@ class ChatInput
             ->createView();
     }
 }
+
