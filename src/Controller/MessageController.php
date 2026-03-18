69ec61872215ed2c0bf28ce4eecd5556fe07c042
Mateusz Nasiadka <matnas2000@wp.pl>
2026-03-18 23:33:57 +0100

    feat(security): add rate limiting for login, registration, and chat
    - Added login_throttling in security firewall.
    - Added registration rate limiter in controller.
    - Added chat_message rate limiter in MessageController.
    - Fixed RateLimiterFactory deprecation in ServerInvitationController.

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -17,6 +17,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\UX\Turbo\TurboBundle;
 use Twig\Error\LoaderError;
@@ -38,7 +39,8 @@ final class MessageController extends AbstractController
         Request $request,
         #[MapEntity(mapping: ['channelId' => 'id', 'serverId' => 'server'])] Channel $channel,
         #[MapEntity(id: 'serverId')] Server $server,
-        MessageService $messageService
+        MessageService $messageService,
+        RateLimiterFactoryInterface $chatMessageLimiter,
     ): Response {
         /** @var User $user */
         $user = $this->getUser();
